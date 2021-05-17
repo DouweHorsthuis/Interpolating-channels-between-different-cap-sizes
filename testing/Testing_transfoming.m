218750a434f1c28463b7b2460faf68913d9527b7
Douwe Horsthuis <douwehorsthuis@gmail.com>
2021-05-17 11:04:06 -0400

    Adding 'landmark' option

--- testing/Testing_transfoming.m
+++ testing/Testing_transfoming.m
@@ -6,7 +6,6 @@ close all
 data_path  = 'C:\Users\dohorsth\Documents\GitHub\Interpolating_160ch_to_64ch_eeglab\testing\';
 cd(data_path) %wherever the script is
 
- EEG = pop_loadset('filename', '160.set', 'filepath', data_path);%loading participant file with 160 channels
- EEG2 = pop_loadset('filename', '64.set', 'filepath', data_path);%loading participant file with 64 channels
- EEG = transform_n_channels(EEG,EEG2.chanlocs,64);
- 
+ EEG = pop_loadset('filename', '64.set', 'filepath', data_path);%loading participant file with 160 channels
+ EEG2 = pop_loadset('filename', '160.set', 'filepath', data_path);%loading participant file with 64 channels
+ EEG = transform_n_channels(EEG,EEG2.chanlocs,160, 'landmark');
